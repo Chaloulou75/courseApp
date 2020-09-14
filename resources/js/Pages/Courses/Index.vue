280d59456cc35fa2ef09b106b865fbc079dc56cd
Chaloulou75 <c.jeandey@gmail.com>
2020-09-14 20:38:59 -0300

    firstbis commit

---
+++ resources/js/Pages/Courses/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <app-layout>
+        <template slot="header">
+            Liste des formations
+        </template>
+        <div v-for="course in courseList" v-bind:key="course.id" class="mx-8 my-4">
+            <div class="px-4 py-2 text-gray-700 bg-green-300 border rounded">
+                <div class="text-base text-cool-gray-600">Mise en ligne par {{ course.user.name }}</div>
+                <div class="flex items-center justify-between">
+                    <div class="text-2xl font-semibold ">{{ course.title }}</div>
+                    <div class="text-sm text-gray-600 underline">{{ course.episodes_count }} episodes</div>
+                </div>
+
+                <div class="text-sm text-gray-600">{{ course.description }}</div>
+                <a :href="'course/' + course.id" class="inline-block px-3 py-1 mt-3 text-sm text-white bg-gray-600 rounded-lg hover:bg-gray-700">Voir la formation</a>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from './../../Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout
+        },
+        data() {
+            return {
+              courseList: this.courses
+            }
+        },
+        props : ['courses']
+    }
+</script>
---
+++ resources/js/Pages/Courses/Show.vue
@@ -0,0 +1,56 @@
+<template>
+    <app-layout>
+        <template slot="header">
+            {{ course.title }}
+        </template>
+        <div class="container mx-8 my-4">
+            <div class="py-4 text-3xl text-gray-600">{{ this.courseShow.episodes[this.currentKey].title }}</div>
+            <iframe class="w-full h-screen" :src="this.courseShow.episodes[this.currentKey].video_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <div class="py-4 text-base text-gray-500">{{ this.courseShow.episodes[this.currentKey].description }}</div>
+
+            <div class="mt-8">
+                <ul v-for="(episode, index) in this.course.episodes" v-bind:key="episode.id">
+                    <li class="mt-3">
+                        Episode n° {{ index +1 }} - {{ episode.title }}
+                        <button class="text-gray-500 focus:outline-none focus:text-gray-400"
+                                @click="switchEpisode(index)">
+                            Voir l'épisode
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from './../../Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout
+        },
+        props : ['course'],
+
+        data() {
+            return {
+                courseShow: this.course,
+                currentKey : 0
+            }
+        },
+
+        methods:{
+            switchEpisode(index){
+                this.currentKey = index;
+                window.scrollTo({
+                    top:0,
+                    left:0,
+                    behavior:'smooth'
+                });
+            }
+        },
+
+
+    }
+</script>
